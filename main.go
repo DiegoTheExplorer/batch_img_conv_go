8a3afc8beb61f80027afe2b95270ffc63b00a4a2
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2025-05-26 12:45:18 +0800

    image decoding by using file extension moved to its own function

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
 	"io/fs"
+	"os"
 	"path"
 	"path/filepath"
 )
@@ -48,7 +51,56 @@ func main() {
 	}
 }
 
+func decodeFromFileExt(img_file *os.File, file_ext string) (image.Image, error) {
+	if file_ext == ".jpeg" || file_ext == ".jpg" {
+		jpg_img, err := jpeg.Decode(img_file)
+
+		if err == nil {
+			return jpg_img, nil
+		}
+		img_file.Seek(0, 0)
+	}
+
+	return nil, fmt.Errorf("File extension does not match image encoding")
+}
+
 func convertToJpeg(fp string, file_extension string, out_dir string) error {
 
-	return fmt.Errorf("error")
+	// Open the file
+	img_file, err := os.Open(fp)
+
+	if err != nil {
+		fmt.Println("Error opening the file: ", fp)
+		return err
+	}
+
+	var decoded_img image.Image
+	file_ext_guess_fail := false
+
+	// Try to use file extension to infer image decoding type
+	if file_extension != "" {
+		decoded_img, err = decodeFromFileExt(img_file, file_extension)
+
+		if err != nil {
+			file_ext_guess_fail = true
+		}
+	}
+
+	if file_ext_guess_fail {
+		// Try all decoders available
+	}
+
+	// concat the filename from fp with out_dir
+	out_filepath := out_dir + filepath.Base(fp)
+
+	// Create file to be written
+	out_file, err := os.Create(out_filepath)
+	if err != nil {
+		fmt.Println("Failed to create output file")
+		return err
+	}
+
+	jpeg.Encode(out_file, decoded_img, &jpeg.Options{Quality: 100})
+
+	return fmt.Errorf("Failed to convert the following file to jpeg: ", fp)
 }
